82302f6d2abe87b75b2297113bfc8a7a15b4a079
MoeMoeee <dangkhoa.140502@gmail.com>
2023-02-09 23:25:17 +0700

    set up router and controller

--- routes/route.js
+++ routes/route.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const {generateImage} = require('../controllers/controller')
+
+router.post('/generateimage', generateImage);
 
 module.exports = router;
