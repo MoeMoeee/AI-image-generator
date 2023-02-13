da0392644f3e443217b40b85ff59a47003f4ba77
MoeMoeee <dangkhoa.140502@gmail.com>
2023-02-13 23:13:39 +0700

    deleted tests in index.js

--- index.js
+++ index.js
@@ -8,12 +8,4 @@ const userRoute = require('./routes/route');
 
 app.use('/openai', userRoute);
 
-app.get('/openai', function(req, res){
-    res.send("Hello from the 'testkaakaka' URL");
-});
-
-app.get('/openai/generateimage', function(req, res){
-    res.send("Hello from the 'moemoe' URL");
-});
-
 app.listen(port, () => console.log(`Server started on port ${port}`));
