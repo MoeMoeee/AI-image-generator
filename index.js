c92e1f4f402878e70b012f0eabf1591ecf984694
MoeMoeee <dangkhoa.140502@gmail.com>
2023-02-15 14:03:40 +0700

    set up to request body data

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const port = process.env.PORT || 4000;
 const app = express();
 const userRoute = require('./routes/route');
 
+//req body data
+//need this to send data
+app.use(express.json()); //parse incoming JSON req into req.body
+app.use(express.urlencoded({extended: false}));
+
 app.use('/openai', userRoute);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
--- routes/route.js
+++ routes/route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {generateImage} = require('../controllers/controller');
 
+//get post request in /generateimage
 router.post('/generateimage', generateImage);
 
 
