8c4b585db633c7ca6c2bf80707a8d4fb0791dae0
MoeMoeee <dangkhoa.140502@gmail.com>
2023-02-16 13:25:30 +0700

    generated image based on the given input and added the waiting message

--- public/js/main.js
+++ public/js/main.js
@@ -11,9 +11,39 @@ function onSubmit(e) {
         return;
     }
 
-    console.log(prompt);
-    console.log(img_size);
+    //generate the image after get the data
+
+    generateImage(prompt, img_size);
 }
+async function generateImage(prompt, size) {
+    try {
+        const response = await fetch('/openai/generateimage', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              prompt,
+              size,
+            }),
+          });
+      
+          if (!response.ok) {
+            throw new Error('That image could not be generated');
+          }
+          
+          document.querySelector('#msg').textContent = 'Your image is generating... Please wait...';
+
+          const res = await response.json();
+          const imageUrl = res.data;
+            
+          document.querySelector('#image_display').src = imageUrl;
+
+    } catch (error) {
+        document.querySelector('#msg').textContent = error;
+    }
+}
+
 document.querySelector('#form').addEventListener('submit', onSubmit);
 
 
